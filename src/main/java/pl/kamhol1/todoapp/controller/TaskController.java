24938dba0615e78235156020e9a0615f06505875
kamhol1 <kamhol271@gmail.com>
2023-04-07 21:25:07 +0200

    REST for tasks

---
+++ src/main/java/pl/kamhol1/todoapp/controller/TaskController.java
@@ -0,0 +1,58 @@
+package pl.kamhol1.todoapp.controller;
+
+import jakarta.validation.Valid;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import pl.kamhol1.todoapp.model.Task;
+import pl.kamhol1.todoapp.model.TaskRepository;
+
+import java.net.URI;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+class TaskController {
+    private static final Logger logger = LoggerFactory.getLogger(TaskController.class);
+    private final TaskRepository repository;
+
+    TaskController(TaskRepository repository) {
+        this.repository = repository;
+    }
+
+    @PostMapping("/tasks")
+    ResponseEntity<Task> createTask(@RequestBody @Valid Task toCreate) {
+        Task result = repository.save(toCreate);
+        return ResponseEntity.created(URI.create("/" + result.getId())).body(result);
+    }
+
+    @GetMapping(value = "/tasks", params = {"!page", "!size", "!sort"})
+    ResponseEntity<List<Task>> readAllTasks() {
+        logger.warn("Exposing all tasks!");
+        return ResponseEntity.ok(repository.findAll());
+    }
+
+    @GetMapping("/tasks")
+    ResponseEntity<List<Task>> readAllTasks(Pageable page) {
+        logger.info("Custom pageable");
+        return ResponseEntity.ok(repository.findAll(page).getContent());
+    }
+
+    @GetMapping("/tasks/{id}")
+    ResponseEntity<Task> readTask(@PathVariable int id) {
+        return repository.findById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PutMapping("/tasks/{id}")
+    ResponseEntity<?> updateTask(@PathVariable int id, @RequestBody @Valid Task toUpdate) {
+        if (!repository.existsById(id))
+            return ResponseEntity.notFound().build();
+
+        repository.save(toUpdate);
+        return ResponseEntity.noContent().build();
+    }
+}
